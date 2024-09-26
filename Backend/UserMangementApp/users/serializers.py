898950cbf0318d0cf2532f160ef5e753c456198b
donjoo <donjorois@gmail.com>
2024-09-26 19:58:58 +0530

    models and serializers

--- Backend/UserMangementApp/users/serializers.py
+++ Backend/UserMangementApp/users/serializers.py
@@ -1,46 +1,33 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
-
+from .models import UserProfile
 
 User = get_user_model()
-
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username'',email','profile_image']
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username','email','password']
-    
+        fields = ('id','first_name','last_name','username','email','phone_number','password','is_active')
+        extra_kwargs = {'password':{'write_only':True}}
 
     def create(self,validated_data):
-        user=User.objects.create_user(
-            username=validated_data['username'],
-            email = validated_data['email'],
-            password=validated_data['password']
-        )
-
+        user = User.objects.create_user(**validated_data)
         return user
     
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
 
-    def validate(self,data):
-        user = User.objects.filter(username=data['username']).first()
-        if user and user.check_password(data['password']):
-            refresh = RefreshToken.for_user(user)
-            return {
-                'refresh':str(refresh),
-                'access':str(refresh.access_token),
-                'user':UserSerializer(user).data
-            }
-        raise serializers.ValidationError("Invalid credentials")
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields =['username','profile_picture'] 
+
+    def update(self, instance, validated_data):
+        user = instance.user
+        user.username = validated_data.get('username',user.username)
+        user.save()
+        instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
+        instance.save()
+        return instance
+    
 
 
 
